7ee31898e3b7a482a56eccb2512ea6f21e5b76e9
ufoism@gmail.com <ufoism@gmail.com@c293fc4f-ed78-beab-0974-2d89754fe5b6>
2012-04-24 17:10:51 +0000

    对服务器中所有类别的牌做了计数

--- trunk/server/src/com/icard/cards/BaseCard.java
+++ trunk/server/src/com/icard/cards/BaseCard.java
@@ -1,6 +1,17 @@
 package com.icard.cards;
 
+import com.icard.cards.cardInfo.CardInfo;
+
 public class BaseCard {
 	
+	public CardInfo cardInfo;//牌的静态信息引用
+	BaseCard(CardInfo info){
+		this.cardInfo = info;
+		this.cardInfo.AddCountOfThisCard();//增加牌的计数
+	}
 	
+	//删除此牌的资源
+	public void Delete(){
+		this.cardInfo.DecCountOfThisCard();//减少牌的计数
+	}
 }
--- trunk/server/src/com/icard/cards/BaseSkill.java
+++ trunk/server/src/com/icard/cards/BaseSkill.java
@@ -1,5 +1,12 @@
 package com.icard.cards;
 
+import com.icard.cards.cardInfo.CardInfo;
+
 public class BaseSkill extends BaseCard {
+
+	BaseSkill(CardInfo info) {
+		super(info);
+		// TODO Auto-generated constructor stub
+	}
 
 }
