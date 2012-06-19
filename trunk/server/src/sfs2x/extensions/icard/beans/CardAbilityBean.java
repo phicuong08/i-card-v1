0fc9fd772c8ee256e2bdb81973edf760c7375f78
zhufanan@gmail.com <zhufanan@gmail.com@c293fc4f-ed78-beab-0974-2d89754fe5b6>
2012-06-19 10:32:37 +0000

    服务器战斗处理

--- trunk/server/src/sfs2x/extensions/icard/beans/CardAbilityBean.java
+++ trunk/server/src/sfs2x/extensions/icard/beans/CardAbilityBean.java
@@ -11,6 +11,7 @@ import sfs2x.extensions.icard.utils.Constants;
  */
 public class CardAbilityBean
 {
+	public static final int WHEN_NULL=0;
 	public static final int WHEN_USE =1; //横置,使用 
 	public static final int WHEN_ALL =2; //持续
 	public static final int WHEN_ENTER =3; //进场
@@ -23,8 +24,64 @@ public class CardAbilityBean
 	public static final int WHEN_DEAD =10; //牌被消灭时
 	public static final int WHEN_DEF_DEAD =11; //牌防御被消灭时
 	public static final int WHEN_ATKED =12; //受到攻击
-	public CardAbilityBean(){
-
+	public static final int WHEN_USE_WEAPON=13;//使用武器
+	public static final int WHEN_RESET=14;     //重置时
+	public static final int WHEN_SIDE=15;      //横置时
+	
+	public static final int BUF_ATK_UNABLE             =1;  //无法攻击
+	public static final int BUF_ATK_ADD                =2;  //攻击力增加
+	public static final int BUF_ATK_DIST               =3;  //远程
+	public static final int BUF_DAMAGE_UNSTOP          =4;  //伤害不可防止
+	public static final int BUF_DEF_UNABLE             =5;  //无法防御
+	public static final int BUF_DIST_WEAPON_ATK_ADD    =6;  //远程武器攻击力增加
+	public static final int BUF_GUIDE                  =7;  //护卫能力
+	public static final int BUF_HIDDEN                 =8;  //潜行
+	public static final int BUF_POINT_UNABLE           =9;  //不可指定
+	public static final int BUF_SHIELD                 =10; //吸收伤害
+	public static final int BUF_UNABLE                 =11; //无法执行
+	public static final int BUF_COST_ADD               =12; //费用增加
+	
+	public static final int DO_NULL           =100;  //
+	public static final int DO_KILL_SKILL     =101;  //消灭技能
+	public static final int DO_DEAD           =102;  //消灭牌
+	public static final int DO_KILL_WEAPON    =103;  //消灭武器
+	public static final int DO_RESET_HERO     =104;  //重置英雄
+	public static final int DO_RESET_WEAPON   =105;  //重置武器
+	public static final int DO_DRAW_CARD      =106;  //抓牌
+	public static final int DO_BREAK_SKILL    =107;  //打断技能
+	public static final int DO_DROP_RES       =108;  //弃资源
+	public static final int DO_DROP_HAND_CARD =109;  //弃手牌
+	public static final int DO_HEAL           =110;  //治疗
+	
+	public static final int OP_OR             =1;
+	public static final int OP_AND            =2;
+	
+	private int _id;
+	private int _when;
+	private int _type;
+	private int _val;
+	private int _op;
+	
+	public CardAbilityBean(int id,int when,int type,int val,int op){
+		_id = id;
+		_when = when;
+		_type = type;
+		_val = val;
+		_op = op;
 	}
-
+	public int getID(){
+		return _id;
+	}
+	public int getWhen(){
+		return _when;
+	}
+	public int getType(){
+		return _type;
+	}
+	public int getVal(){
+		return _val;
+	}
+	public int getOP(){
+		return _op;
+	}
 }
---
+++ trunk/server/src/sfs2x/extensions/icard/beans/CardAbilityStoreBean.java
@@ -0,0 +1,148 @@
+package sfs2x.extensions.icard.beans;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.icard.data.excel.ExcelReader;
+
+public class CardAbilityStoreBean {
+	private final static String CARD_FILE_PATH ="extensions/ICard/cardAbility.xls";
+
+	
+	private static CardAbilityStoreBean mInstance =null;
+	private HashMap<Integer, CardAbilityBean> _abilityInfoMap =new HashMap<Integer, CardAbilityBean>();
+	private CardAbilityStoreBean(){
+	}
+	
+	public static CardAbilityStoreBean GetInstance(){
+		if(mInstance ==null){
+			mInstance = new CardAbilityStoreBean();
+			mInstance.init();
+		}
+		return mInstance;
+	}
+	private void  init(){
+		HashMap<String,ArrayList<HashMap<String,String>>> excelCardData = ExcelReader.loadFile(CARD_FILE_PATH);
+		loadCardExcel(excelCardData);
+	}
+	private void loadCardExcel(HashMap<String,ArrayList<HashMap<String,String>>> excelCardData){
+		for(String sheetName : excelCardData.keySet() ){
+			
+			ArrayList<HashMap<String,String>> sheetData = excelCardData.get(sheetName);
+			for(HashMap<String,String> rowData :sheetData){
+				int Id = Integer.parseInt(rowData.get("ID")); // ÅÆµÄid
+				String whenInfo = (rowData.get("WHEN")==null||rowData.get("WHEN").isEmpty())?"":rowData.get("WHEN"); // 
+				String whatInfo = (rowData.get("WHAT")==null||rowData.get("WHAT").isEmpty())?"":rowData.get("WHAT"); //
+				int val = (rowData.get("VAL")==null||rowData.get("VAL").isEmpty())?"":rowData.get("VAL"); //
+				String op = (rowData.get("OP")==null||rowData.get("OP").isEmpty())?"":rowData.get("OP");//
+				AddAbilityInfo(Id,whenInfo,whatInfo,val,op);
+			}
+		}
+	}
+
+	public Vector<CardAbilityBean> getCardAbility(int cardID){
+		Vector<CardAbilityBean> vec = new Vector<CardAbilityBean>();
+		int pos=0;
+		do{
+			CardAbilityBean ability = _abilityInfoMap.get(cardID*10+pos);
+			if(ability==null)
+				break;
+			vec.add(ability);
+			pos++;	
+		}
+		return vec;
+	}
+	private void AddAbilityInfo(int id,String whenInfo,String whatInfo,int val,String op){
+		CardAbilityBean ability = new CardAbilityBean(Id,  ParseWhenInfo(whenInfo),ParseWhatInfo(whatInfo),
+						  						val,  ParseOpInfo(op));
+		_abilityInfoMap.put(Id, ability);
+	}
+	private int ParseOpInfo(String op){
+		if(op=="OP_OR")
+			return CardAbilityBean.OP_OR;
+		else if(op=="OP_AND")
+			return CardAbilityBean.OP_AND;
+		return 	CardAbilityBean.OP_OR;
+	}
+	private int ParseWhatInfo(String whatInfo){
+		if(whatInfo=="BUF_ATK_UNABLE")
+			return CardAbilityBean.BUF_ATK_UNABLE;
+		else if(whatInfo=="BUF_ATK_ADD")
+			return CardAbilityBean.BUF_ATK_ADD;
+		else if(whatInfo=="BUF_ATK_DIST")
+			return CardAbilityBean.BUF_ATK_DIST;
+		else if(whatInfo=="BUF_DAMAGE_UNSTOP")
+			return CardAbilityBean.BUF_DAMAGE_UNSTOP;
+		else if(whatInfo=="BUF_DEF_UNABLE")
+			return CardAbilityBean.BUF_DEF_UNABLE;
+		else if(whatInfo=="BUF_DIST_WEAPON_ATK_ADD")
+			return CardAbilityBean.BUF_DIST_WEAPON_ATK_ADD;
+		else if(whatInfo=="BUF_GUIDE")
+			return CardAbilityBean.BUF_GUIDE;			
+		else if(whatInfo=="BUF_HIDDEN")
+			return CardAbilityBean.BUF_HIDDEN;
+		else if(whatInfo=="BUF_POINT_UNABLE")
+			return CardAbilityBean.BUF_POINT_UNABLE;
+		else if(whatInfo=="BUF_SHIELD")
+			return CardAbilityBean.BUF_SHIELD;
+		else if(whatInfo=="BUF_UNABLE")
+			return CardAbilityBean.BUF_UNABLE;
+		else if(whatInfo=="BUF_COST_ADD")
+			return CardAbilityBean.BUF_COST_ADD;	
+		else if(whatInfo=="DO_KILL_SKILL")
+			return CardAbilityBean.DO_KILL_SKILL;
+		else if(whatInfo=="DO_DEAD")
+			return CardAbilityBean.DO_DEAD;
+		else if(whatInfo=="DO_KILL_WEAPON")
+			return CardAbilityBean.DO_KILL_WEAPON;
+		else if(whatInfo=="DO_RESET_HERO")
+			return CardAbilityBean.DO_RESET_HERO;
+		else if(whatInfo=="DO_RESET_WEAPON")
+			return CardAbilityBean.DO_RESET_WEAPON;
+		else if(whatInfo=="DO_DRAW_CARD")
+			return CardAbilityBean.DO_DRAW_CARD;		
+		else if(whatInfo=="DO_BREAK_SKILL")
+			return CardAbilityBean.DO_BREAK_SKILL;		
+		else if(whatInfo=="DO_DROP_RES")
+			return CardAbilityBean.DO_DROP_RES;		
+		else if(whatInfo=="DO_DROP_HAND_CARD")
+			return CardAbilityBean.DO_DROP_HAND_CARD;	
+		else if(whatInfo=="DO_HEAL")
+			return CardAbilityBean.DO_HEAL;											
+		return CardAbilityBean.DO_NULL;	 	
+	}
+	private int ParseWhenInfo(String whenInfo){
+		if(whenInfo=="WHEN_USE")
+			return CardAbilityBean.WHEN_USE;
+		else if(whenInfo=="WHEN_ALL")
+			return CardAbilityBean.WHEN_ALL;
+		else if(whenInfo=="WHEN_ENTER")
+			return CardAbilityBean.WHEN_ENTER;
+		else if(whenInfo=="WHEN_ATK")
+			return CardAbilityBean.WHEN_ATK;
+		else if(whenInfo=="WHEN_ATK_SIDE")
+			return CardAbilityBean.WHEN_ATK_SIDE;
+		else if(whenInfo=="WHEN_ATK_HERO_DAMAGE")
+			return CardAbilityBean.WHEN_ATK_HERO_DAMAGE;
+		else if(whenInfo=="WHEN_ATK_DAMAGE")
+			return CardAbilityBean.WHEN_ATK_DAMAGE;
+		else if(whenInfo=="WHEN_MY_LOOP_BEGIN")
+			return CardAbilityBean.WHEN_MY_LOOP_BEGIN;
+		else if(whenInfo=="WHEN_USE_SKILL")
+			return CardAbilityBean.WHEN_USE_SKILL;
+		else if(whenInfo=="WHEN_DEAD")
+			return CardAbilityBean.WHEN_DEAD;
+		else if(whenInfo=="WHEN_DEF_DEAD")
+			return CardAbilityBean.WHEN_DEF_DEAD;
+		else if(whenInfo=="WHEN_ATKED")
+			return CardAbilityBean.WHEN_ATKED;
+		else if(whenInfo=="WHEN_USE_WEAPON")
+			return CardAbilityBean.WHEN_USE_WEAPON;
+		else if(whenInfo=="WHEN_RESET")
+			return CardAbilityBean.WHEN_RESET;
+		else if(whenInfo=="WHEN_SIDE")
+			return CardAbilityBean.WHEN_SIDE;
+		return CardAbilityBean.WHEN_NULL;											
+	}
+}
